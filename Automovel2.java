07cb36fa50a341e15418eed052c2a6b06e0e90f9
Vitor Satake <117522736+VitorSatake@users.noreply.github.com>
2023-03-01 08:59:12 -0300

    Update Automovel2.java

--- Automovel2.java
+++ Automovel2.java
@@ -1,1 +1,25 @@
-// refatorando classe Automovel 
+// refatorando classe Automovel com entrada de dados do usuario
+
+
+package pooVitor;
+
+public class Automovel { 
+
+	// definiçao dos atributos 	 
+
+	String marca; 
+	String modelo; 
+	String cor; 
+	double ano_fabricacao; 
+	double preco; 
+	
+	public void Informa_Automovel (){
+		
+		System.out.println("Marca do automovel: \t " + marca + ".");
+		System.out.println("Tipo do automovel: \t " + modelo + "."); 
+		System.out.println("Cor do automovel: \t " + cor + ".");
+		System.out.println("Ano de fabricação do automovel: \t " + ano_fabricacao + ".");
+		System.out.println("Preco do automovel: \tR$ " + preco + "."); 
+	}
+
+}
