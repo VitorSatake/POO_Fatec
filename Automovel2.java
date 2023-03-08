074d9d710de77cf9151fbc32c35c7a71043febaf
Vitor Satake <117522736+VitorSatake@users.noreply.github.com>
2023-03-08 08:53:49 -0300

    Update Automovel2.java

--- Automovel2.java
+++ Automovel2.java
@@ -23,3 +23,69 @@ public class Automovel {
 	}
 
 }
+
+// Refatorado encapsulando usando set/get
+
+package Contribuinte;
+
+public class Automovel { 
+
+	// definiçao dos atributos 	 
+
+	private String marca; 
+	private String modelo; 
+	private String cor; 
+	private double ano_fabricacao; 
+	private double preco; 
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public void setAno_Fabricacao(double ano_fabricacao) {
+		this.ano_fabricacao = ano_fabricacao;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public double getAno_Fabricacao() {
+		return ano_fabricacao;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	/*
+	public void Informa_Automovel (){
+		
+		System.out.println("Marca do automovel: \t " + marca + ".");
+		System.out.println("Tipo do automovel: \t " + modelo + "."); 
+		System.out.println("Cor do automovel: \t " + cor + ".");
+		System.out.println("Ano de fabricação do automovel: \t " + ano_fabricacao + ".");
+		System.out.println("Preco do automovel: \tR$ " + preco + "."); 
+	}
+*/
+}
