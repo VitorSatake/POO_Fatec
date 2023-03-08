c040b63ba575c3cbb6ef71aa69f30ad3d62c4d6f
Vitor Satake <117522736+VitorSatake@users.noreply.github.com>
2023-03-08 08:59:31 -0300

    Update Manipula_automovel2.java

--- Manipula_automovel2.java
+++ Manipula_automovel2.java
@@ -33,3 +33,47 @@ public class Concessionaria {
 		
 	}
 }
+
+// Refatorado usando set/get com entrada de dados do usuario
+
+package Contribuinte;
+
+import javax.swing.JOptionPane; // biblioteca para trabalhar com janelas de entrada de dados do usuario
+
+
+
+public class Manipula_Automovel {
+	public static void main (String args []) {
+		Automovel e = new Automovel ();
+		
+		String mr, md, pr, cr, ano;
+		double pc,an;
+		
+		
+		mr = JOptionPane.showInputDialog("Digite a marca: ");
+		md = JOptionPane.showInputDialog("Digite o modelo: ");
+		cr = JOptionPane.showInputDialog("Digite a cor: ");
+		ano = JOptionPane.showInputDialog("Digite o ano: ");
+		pr = JOptionPane.showInputDialog("Digite o preço: ");
+		
+		pc = Double.parseDouble(pr); // transforma a string passado em numero real
+		an = Double.parseDouble(ano); // transforma a string passado em numero real
+		
+		e.setMarca(mr);
+		e.setModelo(md);
+		e.setCor(cr);
+		e.setPreco(pc);
+		e.setAno_Fabricacao(an);
+		
+		//e.Informa_Automovel();
+		
+			
+			System.out.println("Marca do automovel: \t " + e.getMarca() + ".");
+			System.out.println("Tipo do automovel: \t " + e.getModelo() + "."); 
+			System.out.println("Cor do automovel: \t " + e.getCor() + ".");
+			System.out.println("Ano de fabricação do automovel: \t " + e.getAno_Fabricacao() + ".");
+			System.out.println("Preco do automovel: \tR$ " + e.getPreco() + "."); 
+		
+		
+	}
+}
