99a8234381da40fdf7e049a3f3fc58f99c006d3c
Vitor Satake <117522736+VitorSatake@users.noreply.github.com>
2023-03-15 08:46:43 -0300

    Add files via upload

---
+++ src/Revendedora1.java
@@ -0,0 +1,11 @@
+
+public class Revendedora1 {
+	public static void main(String args[]) {
+		
+		Automovel a = new Automovel();
+		a.mostra_carro();
+		
+		Automovel b = new Automovel(1977, "Volkswagem", "Fusca", 3888.00);
+		b.mostra_carro();
+	}
+}
